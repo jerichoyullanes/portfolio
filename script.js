31ed8725c0a7206700e7fbb73b3f04e955d47030
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-04-26 21:11:33 +0800

    Added responsiveness

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function downloadCV () {
     window.location.href = "https://drive.google.com/uc?id=1OpIWHB-Z_6o1cvLLSW3EpW-eAgdjtSUk&export=download";
 }
 
+function mobileNav() {
+    console.log("TEST");
+    var x = document.getElementById("mobile-nav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
