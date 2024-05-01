4e5ee62bd8df522700f3cdc1595233d080aca494
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-05-02 05:21:58 +0800

    Added EmailJS and Fixed Send Email Function

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function downloadCV () {
 }
 
 function mobileNav() {
-    console.log("TEST");
     var x = document.getElementById("mobile-nav");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -15,3 +14,34 @@ function mobileNav() {
 function href(link) {
   window.open(`${link}`, "_blank");
 }
+
+const serviceID = "service_m64jjhl";
+const templateID = "template_pap5qf1";
+
+function sendEmail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs.send(serviceID,templateID,params)
+    .then(
+      res => {
+        document.getElementById("name").value = "",
+        document.getElementById("email").value = "",
+        document.getElementById("subject").value = "",
+        document.getElementById("message").value = "",
+        console.log(res);
+        // alert("your email sent successfully");
+        Swal.fire({
+          title: "Email Sent!",
+          text: "Your email is successfully sent!",
+          icon: "success"
+        });
+      }
+    )
+    .catch((err) => console.log(err));
+}
+
